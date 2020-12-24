d3110adc2dbe525262945d1bd6931c83c5dd3def
josep <josep@DESKTOP-4QRB039.mshome.net>
2020-12-24 10:35:12 -0600

    24/12/2020 - Se agregó la clase AlumnoClass

---
+++ src/proyectofinaledd/AlumnoClass.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectofinaledd;
+
+/**
+ *
+ * @author josep
+ */
+public class AlumnoClass {
+    //Esta es la clase del Objeto Alumno.
+    //Se ha puesto como arreglo de strings Materias pues son 6 materias que se tienen que almacenar. Si tienen otra idea hagan cambios y expliquen en el was xd
+    int NoControl;
+    String Nombres, ApPat, ApMat;
+    int Semestre;
+    String Carrera, Materias[];
+    int Calif[], Status[], PromGral;
+
+    public AlumnoClass(int NoControl, String Nombres, String ApPat, String ApMat, int Semestre, String Carrera, String[] Materias, int[] Calif, int[] Status, int PromGral) {
+        this.NoControl = NoControl;
+        this.Nombres = Nombres;
+        this.ApPat = ApPat;
+        this.ApMat = ApMat;
+        this.Semestre = Semestre;
+        this.Carrera = Carrera;
+        this.Materias = Materias;
+        this.Calif = Calif;
+        this.Status = Status;
+        this.PromGral = PromGral;
+    }
+
+    public int getNoControl() {
+        return NoControl;
+    }
+
+    public void setNoControl(int NoControl) {
+        this.NoControl = NoControl;
+    }
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getApPat() {
+        return ApPat;
+    }
+
+    public void setApPat(String ApPat) {
+        this.ApPat = ApPat;
+    }
+
+    public String getApMat() {
+        return ApMat;
+    }
+
+    public void setApMat(String ApMat) {
+        this.ApMat = ApMat;
+    }
+
+    public int getSemestre() {
+        return Semestre;
+    }
+
+    public void setSemestre(int Semestre) {
+        this.Semestre = Semestre;
+    }
+
+    public String getCarrera() {
+        return Carrera;
+    }
+
+    public void setCarrera(String Carrera) {
+        this.Carrera = Carrera;
+    }
+
+    public String[] getMaterias() {
+        return Materias;
+    }
+
+    public void setMaterias(String[] Materias) {
+        this.Materias = Materias;
+    }
+
+    public int[] getCalif() {
+        return Calif;
+    }
+
+    public void setCalif(int[] Calif) {
+        this.Calif = Calif;
+    }
+
+    public int[] getStatus() {
+        return Status;
+    }
+
+    public void setStatus(int[] Status) {
+        this.Status = Status;
+    }
+
+    public int getPromGral() {
+        return PromGral;
+    }
+
+    public void setPromGral(int PromGral) {
+        this.PromGral = PromGral;
+    }
+}
