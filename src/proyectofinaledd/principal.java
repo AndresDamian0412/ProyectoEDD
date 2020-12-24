4c15c3bb3076f4a4a9c686232caa11888144c59b
andre <andre@LAPTOP-A5R0K1L6>
2020-12-24 17:35:13 -0600

    Agregué el menú, y avancé en la parte de la primera opción En teoría ya debe ordenar el arreglo de alumnos, falta mostrar los datos ordenados en pantalla

--- src/proyectofinaledd/principal.java
+++ src/proyectofinaledd/principal.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
  * @author josep
  */
 public class principal {
+
     static Scanner scanner = new Scanner(System.in);
     //Este es el arreglo de objetos sin tamaño
     static AlumnoClass alumnos[];
@@ -34,7 +35,101 @@ public class principal {
             GuardarDatos(incremento);//Pasamos por parametro el valor de la variable que incrementa en cada vuelta
             incremento++;
         }
+        Menu();
 
+    }
+
+    public static void Menu() {
+        int op = 0;
+        System.out.println("=====SISTEMA DE INFORMACIÓN DE ALUMNOS NIVEL MEDIO SUPERIOR=====");
+        System.out.println("Seleccione una de las siguienes opciones");
+        System.out.println("1) Listado de alumnos");
+        System.out.println("2) Consultar alumnos por carrera");
+        System.out.println("3) Alumnos candidatos a baja por reprobación");
+        System.out.println("4) Alumnos que recursarán alguna materia");
+        System.out.println("5) Alumnos que llevarán alguna materia en especial");
+        System.out.println("6) Alumnos con el promedio más bajo");
+        System.out.println("7) Alumnos con el primedio más alto");
+        System.out.println("8) Alumnos candidatos a beca");
+        System.out.println("9) Salir");
+
+        switch (op) {
+            case 1: {
+                System.out.println("La lista original de alumnos ordenados por apellido es: ");
+                OrdenaAlumnosApe(alumnos,0,(alumnos.length-1));//ordena los objetos alumno por apellido paterno
+                //sigue mostrarlos
+                break;
+            }
+            case 2: {
+
+                break;
+            }
+            case 3: {
+
+                break;
+            }
+            case 4: {
+
+                break;
+            }
+            case 5: {
+
+                break;
+            }
+            case 6: {
+
+                break;
+            }
+            case 7: {
+
+                break;
+            }
+            case 8: {
+
+                break;
+            }
+            case 9: {
+                System.out.println("GRACIAS POR UTILIZAR EL SISTEMA");
+                break;
+            }
+            default: {
+                System.out.println("Ingrese una opción válida");
+                Menu();
+                break;
+            }
+        }
+    }
+
+    public static void OrdenaAlumnosApe(AlumnoClass[] arreglo, int izq, int der) {
+
+        AlumnoClass pivote = arreglo[izq]; // tomamos primer elemento como pivote
+        int i = izq;         // i realiza la búsqueda de izquierda a derecha
+        int j = der;         // j realiza la búsqueda de derecha a izquierda
+        AlumnoClass aux;
+
+        while (i < j) {                          // mientras no se crucen las búsquedas                                   
+            while (arreglo[i].getApPat().compareTo(pivote.getApPat()) <= 0 && i < j) {
+                i++; // busca elemento mayor que pivote
+            }
+            while (arreglo[j].getApPat().compareTo(pivote.getApPat()) > 0) {
+                j--;           // busca elemento menor que pivote
+            }
+            if (i < j) {                        // si no se han cruzado                      
+                aux = arreglo[i];                      // los intercambia
+                arreglo[i] = arreglo[j];
+                arreglo[j] = aux;
+            }
+        }
+
+        arreglo[izq] = arreglo[j];      // se coloca el pivote en su lugar de forma que tendremos                                    
+        arreglo[j] = pivote;      // los menores a su izquierda y los mayores a su derecha
+
+        if (izq < j - 1) {
+            OrdenaAlumnosApe(arreglo, izq, j - 1);          // ordenamos subarray izquierdo
+        }
+        if (j + 1 < der) {
+            OrdenaAlumnosApe(arreglo, j + 1, der);      // ordenamos subarray derecho
+        }
     }
 
     //Aqui esta el metodo que pide los registros de los alumnos y lo almacena en el arreglo de objetos
@@ -42,7 +137,7 @@ public class principal {
         String materias[] = new String[6];
         int Calif[] = new int[6];
         int Status[] = new int[6];
-        
+
         //Pide los datos
         System.out.println("Ingrese numero de control: ");
         int nControl = Integer.parseInt(scanner.nextLine());
