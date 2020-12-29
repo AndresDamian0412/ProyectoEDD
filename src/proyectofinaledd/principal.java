a02498700de0c46435d09c45eb1ccbb13399be13
josep <josep@DESKTOP-4QRB039.mshome.net>
2020-12-29 14:30:10 -0600

    29/12/2020 - Se modifico la clase 'principal': Se realizaron cambios al metodo de busqueda binaria de strings. Y se añadieron comentarios al respecto.

--- src/proyectofinaledd/principal.java
+++ src/proyectofinaledd/principal.java
@@ -5,6 +5,7 @@
  */
 package proyectofinaledd;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -20,111 +21,133 @@ public class principal {
 
     public static void Menu() {
 
-        System.out.println("=====SISTEMA DE INFORMACIÓN DE ALUMNOS NIVEL MEDIO SUPERIOR=====");
-        System.out.println("Seleccione una de las siguienes opciones");
-        System.out.println("1) Listado de alumnos");
-        System.out.println("2) Consultar alumnos por carrera");
-        System.out.println("3) Alumnos candidatos a baja por reprobación");
-        System.out.println("4) Alumnos que recursarán alguna materia");
-        System.out.println("5) Alumnos que llevarán alguna materia en especial");
-        System.out.println("6) Alumnos con el promedio más bajo");
-        System.out.println("7) Alumnos con el primedio más alto");
-        System.out.println("8) Alumnos candidatos a beca");
-        System.out.println("9) Salir");
-        
-        System.out.println("Ingrese una opcion: ");
-        int op = Integer.parseInt(scanner.nextLine());
-        
-        switch (op) {
-            case 1: {
-                System.out.println("La lista original de alumnos ordenados por apellido es: ");
-                OrdenaAlumnosApe(alumnos, 0, (alumnos.length - 1));//ordena los objetos alumno por apellido paterno
-                //sigue mostrarlos // Ya los muestra ;) Pero falta darle forma, es temporal la muestra de datos
-                for (AlumnoClass alumno : alumnos) {
-                    System.out.println(alumno.getNombres() + " " + alumno.getApPat() + " " + alumno.getApMat()
-                    + " " + alumno.getNoControl() + " " + alumno.getCarrera() + " " + alumno.getSemestre() +
-                            " " + alumno.getPromGral());
+        boolean repetir = true;
+        while (repetir) {
+            System.out.println("=====SISTEMA DE INFORMACIÓN DE ALUMNOS NIVEL MEDIO SUPERIOR=====");
+            System.out.println("Seleccione una de las siguienes opciones");
+            System.out.println("1) Listado de alumnos");
+            System.out.println("2) Consultar alumnos por carrera");
+            System.out.println("3) Alumnos candidatos a baja por reprobación");
+            System.out.println("4) Alumnos que recursarán alguna materia");
+            System.out.println("5) Alumnos que llevarán alguna materia en especial");
+            System.out.println("6) Alumnos con el promedio más bajo");
+            System.out.println("7) Alumnos con el primedio más alto");
+            System.out.println("8) Alumnos candidatos a beca");
+            System.out.println("9) Salir");
+
+            System.out.println("Ingrese una opcion: ");
+            int op = Integer.parseInt(scanner.nextLine());
+
+            switch (op) {
+                case 1: {
+                    System.out.println("La lista original de alumnos ordenados por apellido es: ");
+                    OrdenaAlumnosApe(alumnos, 0, (alumnos.length - 1));//ordena los objetos alumno por apellido paterno
+                    //sigue mostrarlos // Ya los muestra ;) Pero falta darle forma, es temporal la muestra de datos
+                    for (AlumnoClass alumno : alumnos) {
+                        System.out.println(alumno.getNombres() + " " + alumno.getApPat() + " " + alumno.getApMat()
+                                + " " + alumno.getNoControl() + " " + alumno.getCarrera() + " " + alumno.getSemestre()
+                                + " " + alumno.getPromGral());
+                    }
+
+                    break;
                 }
-                break;
-            }
-            
-            case 2: {
-                //Este espacio esta modificado debido a que no se aplicaba la busqueda binaria **AUN ESTA EN PROCESO**
-                System.out.println("Ingrese carrera: ");
-                String carrera = scanner.nextLine();
-                System.out.println("========Alumnos de " + carrera + "========");
-                
-                String[] Carreras = new String[alumnos.length];
-                int in = 0;
-                for (AlumnoClass alumno : alumnos) {
-                    Carreras[in] = alumno.getCarrera();
-                    in++;
+
+                case 2: {
+                    //Este espacio esta modificado debido a que no se aplicaba la busqueda binaria **AUN ESTA EN PROCESO**
+                    System.out.println("Ingrese carrera: ");
+                    String carrera = scanner.nextLine();
+                    System.out.println("========Alumnos de " + carrera + "========");
+
+                    String[] Carreras = new String[alumnos.length];
+                    int in = 0;
+                    for (AlumnoClass alumno : alumnos) {
+                        Carreras[in] = alumno.getCarrera();
+                        in++;
+                    }
+                    for (String Carrera : Carreras) {
+                        System.out.println(Carrera);
+                    }
+
+                    //int posicion = busquedaBinariaRecursiva(Carreras, carrera, 0, Carreras.length-1);
+                    
+                    //AVISO: POR ALGUNA RAZÓN LA POSICION [0] DEL ARRAY NO ES TOMADA EN CUENTA Y ES COMO SI NO HUBIERA VALOR
+                    //PERO LAS DEMAS POSICIONES SI SON CONOCIDAS. PARECE QUE SOLO ES ESE EL UNICO FALLO QUE HE ENCOTRADO
+                    //EL METODO FUNCIONA PERFECTAMENTE Y SOLO FALTA APLICARLO DE MANERA CORRECTA AL PROBLEMA
+                    //EL METODO FUE CAMBIADO DE RECURSIVO A ITERATIVO (USANDO WHILE) PARA CHECAR SI ESE ERA EL FALLO.
+                    //EL CODIGO ORIGINAL AQUI: https://parzibyte.me/blog/2018/10/31/busqueda-binaria-java-arreglos-cadenas/
+                    System.out.println("Posicion: " + Arrays.binarySearch(Carreras, carrera));
+                    System.out.println("Posicion con metodo: " + busquedaBinariaConWhile(Carreras, carrera));
+                    break;
                 }
-                int posicion = busquedaBinariaRecursiva(Carreras, carrera, 0, Carreras.length-1);
-                break;
-            }
-            case 3: {
-                
-                break;
-            }
-            case 4: {
+                case 3: {
 
-                break;
-            }
-            case 5: {
+                    break;
+                }
+                case 4: {
 
-                break;
-            }
-            case 6: {
+                    break;
+                }
+                case 5: {
 
-                break;
-            }
-            case 7: {
+                    break;
+                }
+                case 6: {
 
-                break;
-            }
-            case 8: {
+                    break;
+                }
+                case 7: {
 
-                break;
-            }
-            case 9: {
-                System.out.println("GRACIAS POR UTILIZAR EL SISTEMA");
-                break;
-            }
-            default: {
-                System.out.println("Ingrese una opción válida");
-                Menu();
-                break;
+                    break;
+                }
+                case 8: {
+
+                    break;
+                }
+                case 9: {
+                    System.out.println("GRACIAS POR UTILIZAR EL SISTEMA");
+                    repetir = false;
+                    break;
+                }
+                default: {
+                    System.out.println("Ingrese una opción válida");
+                    Menu();
+                    break;
+                }
             }
         }
     }
-    
-        
+
     //Metodo de busqueda binaria con Strings
-    public static int busquedaBinariaRecursiva(String[] array, String busqueda, int izquierda, int derecha){
-        //Si izquierda es mayor que derecha significa que no encontramos nada
-        if(izquierda > derecha){
-            return -1;
+    public static int busquedaBinariaConWhile(String[] arreglo, String busqueda) {
+
+        int izquierda = 0, derecha = arreglo.length - 1;
+
+        while (izquierda <= derecha) {
+            // Calculamos las mitades...
+            int indiceDelElementoDelMedio = (int) Math.floor((izquierda + derecha) / 2);
+            String elementoDelMedio = arreglo[indiceDelElementoDelMedio];
+
+            // Primero vamos a comparar y ver si el resultado es negativo, positivo o 0
+            int resultadoDeLaComparacion = busqueda.compareToIgnoreCase(elementoDelMedio);
+
+            // Si el resultado de la comparación es 0, significa que ambos elementos son iguales
+            // y por lo tanto quiere decir que hemos encontrado la búsqueda
+            if (resultadoDeLaComparacion == 0) {
+                return indiceDelElementoDelMedio;
+            }
+
+            // Si no, entonces vemos si está a la izquierda o derecha
+            if (resultadoDeLaComparacion < 0) {
+                derecha = indiceDelElementoDelMedio - 1;
+            } else {
+                izquierda = indiceDelElementoDelMedio + 1;
+            }
         }
-        //Calculamos las mitades del arreglo
-        int mitad = (int) Math.floor((izquierda+derecha)/2);
-        String elemtMid = array[mitad];
-        int comparacion = busqueda.compareTo(elemtMid);
-        
-        if(comparacion == 0){
-            return mitad;
-        }
-        if(comparacion < 0 ){
-            derecha = mitad-1;
-            return busquedaBinariaRecursiva(array, busqueda, izquierda, derecha);
-        }else{
-            izquierda = mitad+1;
-            return busquedaBinariaRecursiva(array, busqueda, izquierda, derecha);
-        }
-        
+        // Si no se rompió el ciclo ni se regresó el índice, entonces el elemento no
+        // existe
+        return -1;
     }
-    
-    
+
     //Ordena los alumnos por apellido paterno mediante quicksort
     public static void OrdenaAlumnosApe(AlumnoClass[] arreglo, int izq, int der) {
 
@@ -157,9 +180,9 @@ public class principal {
             OrdenaAlumnosApe(arreglo, j + 1, der);      // ordenamos subarray derecho
         }
     }
-    
+
     //Ordena los alumnos por numero de control mediante quicksort
-    public static void OrdenaAlumnosNumC(AlumnoClass[] arreglo, int izq, int der){
+    public static void OrdenaAlumnosNumC(AlumnoClass[] arreglo, int izq, int der) {
         AlumnoClass pivote = arreglo[izq]; // tomamos primer elemento como pivote
         int i = izq;         // i realiza la búsqueda de izquierda a derecha
         int j = der;         // j realiza la búsqueda de derecha a izquierda
@@ -189,10 +212,10 @@ public class principal {
             OrdenaAlumnosNumC(arreglo, j + 1, der);      // ordenamos subarray derecho
         }
     }
-    
+
     //Ordena los alumnos por cantidad de materias en especial mediante quicksort
     //Se usa este metodo para el inciso C
-    public static void OrdenaAlumnosMatEsp(AlumnoClass[] arreglo, int izq, int der){
+    public static void OrdenaAlumnosMatEsp(AlumnoClass[] arreglo, int izq, int der) {
         AlumnoClass pivote = arreglo[izq]; // tomamos primer elemento como pivote
         int i = izq;         // i realiza la búsqueda de izquierda a derecha
         int j = der;         // j realiza la búsqueda de derecha a izquierda
@@ -228,7 +251,7 @@ public class principal {
         String materias[] = new String[6];
         int Calif[] = new int[6];
         int Status[] = new int[6];
-        int ContEsp=0; //Contador que nos dice cuantas materias en especial tiene "x" alumno
+        int ContEsp = 0; //Contador que nos dice cuantas materias en especial tiene "x" alumno
 
         //Pide los datos
         System.out.println("Ingrese numero de control: ");
@@ -258,8 +281,9 @@ public class principal {
         for (int i = 0; i < 6; i++) {
             System.out.println("Ingrese status de materia [" + (i + 1) + "] :");
             Status[i] = Integer.parseInt(scanner.nextLine());
-            if(Status[i]==3)
+            if (Status[i] == 3) {
                 ContEsp++;   //Si la materia esta en especial, el contador se incrementa
+            }
         }
         //Calculamos promedio en base al arreglo de calificaciones
         int x = 0;
@@ -269,39 +293,38 @@ public class principal {
         int Prom = x / 6;
         //Se guardan los datos en el arreglo de objetos, la posicion depende del valor que se le pasa
         //por parametro
-        alumnos[incremento] = new AlumnoClass(nControl, Nombre, ApPat, ApMat, Semestre, Carrera, materias, Calif, Status, Prom,ContEsp);
+        alumnos[incremento] = new AlumnoClass(nControl, Nombre, ApPat, ApMat, Semestre, Carrera, materias, Calif, Status, Prom, ContEsp);
     }
-    
+
     //metodo de búsqueda binaria, **********falta acoplar a nuestro proyecto*********
     public static int binarySearch(int[] array, int minLimit, int maxLimit, int value) {
         if (maxLimit >= 0 && array[minLimit] <= value && array[maxLimit] >= value) {
-            int mid = (minLimit+ maxLimit)/2;
-            System.out.println(String.format("Límite inferior %d límite superior %d valor en el arreglo %d valor a buscar %d", minLimit,maxLimit,array[mid],value));
+            int mid = (minLimit + maxLimit) / 2;
+            System.out.println(String.format("Límite inferior %d límite superior %d valor en el arreglo %d valor a buscar %d", minLimit, maxLimit, array[mid], value));
             if (array[mid] == value) {
                 return mid;
-            } else if (array[mid] < value){
+            } else if (array[mid] < value) {
                 return binarySearch(array, mid + 1, maxLimit, value);
             }
             return binarySearch(array, minLimit, mid - 1, value);
         }
         return -1;
     }
-    
 
-    public static AlumnoClass[] eliminaElemento(AlumnoClass[] arreglo, int posicion){
-        AlumnoClass[] aux = new AlumnoClass[arreglo.length-1];
+    public static AlumnoClass[] eliminaElemento(AlumnoClass[] arreglo, int posicion) {
+        AlumnoClass[] aux = new AlumnoClass[arreglo.length - 1];
         for (int i = 0; i < arreglo.length; i++) {
-            if(i!=posicion){                //éste método únicamente nos sirve para eliminar
-                aux[i]= arreglo[i];         // los alumnos reprobados que vayamos pasando al arreglo
+            if (i != posicion) {                //éste método únicamente nos sirve para eliminar
+                aux[i] = arreglo[i];         // los alumnos reprobados que vayamos pasando al arreglo
             }
         }
         return aux;
     }
-    
-    public void MetodoParaPruebas(){
+
+    public void MetodoParaPruebas() {
         //clase auxiliar para probar código
     }
-    
+
     public static void main(String[] args) {
 
         //Se pide el tamaño del arreglo [Esto lo hice de forma temporal por si no habia otra manera
